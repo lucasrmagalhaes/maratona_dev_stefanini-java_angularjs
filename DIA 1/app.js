15171aaec30c3c4049c77412adc63a56b6191fda
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2020-10-06 01:26:59 -0300

    ng-app, controller, model, bind e click

---
+++ DIA 1/app.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict"; // garantirando segurança
+    
+    angular
+        .module('app', []) // contém todos os recursos do framework
+        .controller('controlador', function($scope){ // traz o contexto da aplicação
+            $scope.nome = 'Lucas';
+            $scope.contador = 10;
+
+        });
+})();
